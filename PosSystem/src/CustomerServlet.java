3e1fecd671254326e2e954b26b863a6d285b41f5
TharinduShantha98 <tharindushantha4@gmail.com>
2022-05-08 10:52:58 -0700

    done the input of index page

--- PosSystem/src/CustomerServlet.java
+++ PosSystem/src/CustomerServlet.java
@@ -1,6 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
-public class CustomerServlet {
 
-    
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello");
+
+    }
+
+
+
 }
