90a9013e082e644932446f301030023e33fd3749
TharinduShantha98 <tharindushantha4@gmail.com>
2022-05-11 00:13:21 -0700

    create get all customer

--- PosSystem/src/controllers/CustomerServlet.java
+++ PosSystem/src/controllers/CustomerServlet.java
@@ -4,12 +4,16 @@ import bo.custom.CustomerBo;
 import bo.custom.Impl.CustomerBoImpl;
 import model.CustomerDTO;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -24,16 +28,37 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.setContentType("application/json");
+
 
         try {
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
             ArrayList<CustomerDTO> allCustomer = customerBo.getAllCustomer();
 
             for (CustomerDTO c1 : allCustomer
                  ) {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("id",c1.getCustomerId());
+                objectBuilder.add("firstName",c1.getFirstName());
+                objectBuilder.add("lastName",c1.getLastName());
+                objectBuilder.add("address",c1.getAddress());
+                objectBuilder.add("email",c1.getEmail());
+                objectBuilder.add("TelNo",c1.getTelNo());
+
+                arrayBuilder.add(objectBuilder.build());
 
 
-                System.out.println(c1.getCustomerId());
+
             }
+
+            PrintWriter writer = resp.getWriter();
+
+            JsonObjectBuilder objectBuilder2 = Json.createObjectBuilder();
+            objectBuilder2.add("data",arrayBuilder.build());
+            objectBuilder2.add("message","done");
+            objectBuilder2.add("status","200");
+            writer.print(objectBuilder2.build());
 
 
 
@@ -47,5 +72,9 @@ public class CustomerServlet extends HttpServlet {
     }
 
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+
+    }
 }
