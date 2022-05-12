e33b970f23fd92a8c15896999c597bda9a532781
TharinduShantha98 <tharindushantha4@gmail.com>
2022-05-12 16:25:42 -0700

    updated item doget method

--- PosSystem/src/controllers/ItemServlet.java
+++ PosSystem/src/controllers/ItemServlet.java
@@ -4,12 +4,16 @@ import bo.custom.Impl.ItemBoImpl;
 import bo.custom.ItemBo;
 import model.ItemDTO;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -23,9 +27,37 @@ public class ItemServlet  extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+
+
         try {
 
             ArrayList<ItemDTO> allItem = itemBo.getAllItem();
+
+            for (ItemDTO itemDTO: allItem){
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("itemCode",itemDTO.getItemCode());
+                objectBuilder.add("itemName",itemDTO.getItemName());
+                objectBuilder.add("unitPrice",itemDTO.getUnitPrice());
+                objectBuilder.add("packSize",itemDTO.getPackSize());
+                objectBuilder.add("buyingPrice",itemDTO.getBuyingPrice());
+                objectBuilder.add("quantity",itemDTO.getQuantity());
+                arrayBuilder.add(objectBuilder.build());
+
+
+
+            }
+
+
+            JsonObjectBuilder objectBuilder2 = Json.createObjectBuilder();
+            objectBuilder2.add("data",arrayBuilder.build());
+            objectBuilder2.add("message","successfully get all items");
+            objectBuilder2.add("status", 200);
+            writer.print(objectBuilder2.build());
 
 
 
